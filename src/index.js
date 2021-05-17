1c363b9667324e137101961dbcbd722678bd7253
wldns0622 <wldns12378@gmail.com>
2021-05-17 22:17:38 +0900

    connect button and showing count

--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@ const number = document.querySelector("span");
 
 const countModifier = (count = 0, action) => {
   if (action.type === "ADD") {
+    console.log("ADD");
     return count + 1;
   }
   else if (action.type === "MINUS") {
+    console.log("MINUS");
     return count - 1;
   } else {
     return count;
@@ -16,13 +18,13 @@ const countModifier = (count = 0, action) => {
 
 };
 
-const countStore = createStore(countModifier)
+const countStore = createStore(countModifier);
 
-countStore.dispatch({ type: "ADD" });
-console.log(countStore.getState());
-countStore.dispatch({ type: "ADD" });
-console.log(countStore.getState());
-countStore.dispatch({ type: "ADD" });
-console.log(countStore.getState());
-countStore.dispatch({ type: "MINUS" });
-console.log(countStore.getState());
+const onChange = () => {
+  number.innerText = countStore.getState();
+}
+
+countStore.subscribe(onChange);
+
+plus.addEventListener("click", () => countStore.dispatch({ type: "ADD" }));
+minus.addEventListener("click", () => countStore.dispatch({ type: "MINUS" }));
